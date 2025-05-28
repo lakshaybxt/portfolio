9be3935406e3732254f3841bf2fef0491a71f214
lakshaybxt <bishtlakshay14@gmail.com>
2025-05-28 17:52:39 +0530

    Final Push

---
+++ lakshay-portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+
+  return <div className="bg-[#0A0C0D] h-auto w-full overflow-hidden">
+    <Navbar/>
+  </div>
+  
+}
+
+export default App;
