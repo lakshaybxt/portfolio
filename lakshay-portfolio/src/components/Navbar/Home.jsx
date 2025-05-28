112d3051bd03e3976d9f94d53614157048931c9f
lakshaybxt <bishtlakshay14@gmail.com>
2025-05-28 19:45:12 +0530

    Added

--- lakshay-portfolio/src/components/Navbar/Home.jsx
+++ lakshay-portfolio/src/components/Navbar/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-white flex w-full justify-between items-start p-10 md:p-20'>
+      <div></div>
+    </div>
   )
 }
 
